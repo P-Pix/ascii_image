3c91062ff7e1269e1ca07bff92930f4c738e86c1
P-Pix <guillaume200202@gmail.com>
2021-03-05 15:29:51 +0100

    projet fini

--- main.cpp
+++ main.cpp
@@ -5,6 +5,10 @@
 - signe choisi pour le pixel = ascii[niveau de gris]
 **/
 
+/**
+- vitesse moyenne = 32 400 pixels par seconde
+**/
+
 #include <iostream>
 #include <fstream>
 #include <string>
@@ -23,70 +27,94 @@ sf::Image open_image(const char *NAME_IMAGE)
     return IMAGE;
 }
 
-void right_in_txt(const char *ASCII_SIGNE, ofstream FICHIER, bool END_LIGNE)
-{
-    if(END_LIGNE)
-    {
-        FICHIER << ASCII_SIGNE << endl;
-    }
-    else
-    {
-        FICHIER << ASCII_SIGNE;
-    }
-}
-
 //recherche actuelle = obtenir le size de l'image
 
 unsigned int receve_width_image(Image IMAGE)
 {
     unsigned int WIDTH;
-    WIDTH = 10;
+    WIDTH = 360;
+    /**
     sf::Vector2u Vector;
     Vector = Image::getSize();
     Vector(IMAGE);
+    **/
     return WIDTH;
 }
 
 unsigned int receve_height_image(Image IMAGE)
 {
     unsigned int HEIGHT;
-    HEIGHT = 10;
+    HEIGHT = 360;
+    /**
     sf::Vector2u Vector;
     Vector = Image::getSize();
     Vector(IMAGE);
+    **/
     return HEIGHT;
+}
+
+float pourcentage(unsigned int VALEUR_ACTUELLE, unsigned int VALEUR_FINALE)
+{
+    float RESULTAT;
+    RESULTAT = (VALEUR_ACTUELLE * 100) / VALEUR_FINALE;
+    return RESULTAT;
 }
 
 
 unsigned int color_one_pixel(Image IMAGE, unsigned int x, unsigned int y)
 {
     sf::Color COLOR;
+    unsigned int GRIS;
     COLOR = IMAGE.getPixel(x, y);
-    return 0;
+    GRIS = (COLOR.r + COLOR.g + COLOR.b) / 3;
+    return GRIS;
+}
+
+const char choix_ascii(unsigned int NIVEAU_GRIS, const char *ASCII)
+{
+    unsigned int NOMBRE_SIGNE_POSSIBLE;
+    const char NON_SIGNE_CHOISI = {' '};
+    NOMBRE_SIGNE_POSSIBLE = 255 / (sizeof(ASCII) / 4);
+    for(unsigned int x = 0; x < sizeof(ASCII) / 4; x ++)
+    {
+        if(NIVEAU_GRIS > NOMBRE_SIGNE_POSSIBLE * x)
+        {
+            if(NIVEAU_GRIS <= NOMBRE_SIGNE_POSSIBLE * (x + 1))
+            {
+                const char SIGNE_CHOISI = ASCII[x];
+                return SIGNE_CHOISI;
+            }
+        }
+    }
+    return NON_SIGNE_CHOISI;
 }
 
 
 int main()
 {
-    unsigned int HEIGHT, WIDTH;
+    unsigned int HEIGHT, WIDTH, NIVEAU_GRIS;
+    float POURCENTAGE;
     const char *NAME_IMAGE = "image/coeur.jpg", ASCII[] = {'a', '.', ' '};
+    string const NAME_FICHIER = "ASCII.txt";
 
     sf::Image IMAGE;
+    ofstream FICHIER(NAME_FICHIER.c_str());
 
     IMAGE = open_image(NAME_IMAGE);
     HEIGHT = receve_height_image(IMAGE);
     WIDTH = receve_width_image(IMAGE);
-    //color_one_pixel(image_loaded);
 
     for(unsigned int y = 0; y < HEIGHT; y ++)
     {
         for(unsigned int x = 0; x < WIDTH; x ++)
         {
-            // appel fonction color et choix ascii
-            // verifier si le x == WIDTH si oui faire le endl
-            // appel fonction right_in_txt
-            cout << "le y = " << y << " et le x = " << x << endl;
+            NIVEAU_GRIS = color_one_pixel(IMAGE, x, y);
+            const char ASCII_SIGNE = choix_ascii(NIVEAU_GRIS, ASCII);
+            FICHIER << ASCII_SIGNE;
         }
+        FICHIER << endl;
+        POURCENTAGE = pourcentage(y * y, HEIGHT * WIDTH);
+        cout << POURCENTAGE << " %" << endl;
     }
 
     return 0;
